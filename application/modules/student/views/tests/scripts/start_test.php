9ce476daa3918be71d550f33b12b98a9bfed1e53
Ankit Shukla <4nkit5hukla@gmail.com>
2020-10-07 17:56:14 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- application/modules/student/views/tests/scripts/start_test.php
+++ application/modules/student/views/tests/scripts/start_test.php
@@ -77,13 +77,14 @@
 		};
 		mediaRecorder = new MediaRecorder(stream, options);
 		recStartTime = moment();
-		mediaRecorder . start(10000);
+		mediaRecorder . start(5000);
 		mediaRecorder.addEventListener('dataavailable', function (e) {
 			if (e.data.size > 0) {
-				fetch(`<?php echo site_url("student/tests_actions/save_chunks"); ?>/${fileName}`, {
+				fetch(`https: //webrtc.inovmercury.com/save-file`, {
 						method: 'POST',
 						headers: {
 							'Content-Type': 'application/octet-stream',
+							fileId: fileName
 						},
 						body: e.data
 					})
